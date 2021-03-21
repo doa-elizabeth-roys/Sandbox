7ab79ec082799023c7345702aac5e07fed895271
Doa Roys <doa.roys@my.jcu.edu.au>
2021-03-21 12:01:04 +1000

    Password checker program

--- password_check.py
+++ password_check.py
@@ -1,8 +1,11 @@
 MIN_LENGTH = 8
-password = input("Enter password:")
-if len(password)<MIN_LENGTH:
+password = input("Enter password: ")
+valid_input = False
+while not valid_input:
+ if len(password)<MIN_LENGTH:
     print("Enter a password with atleast {} characters".format(MIN_LENGTH))
-    password = input ( "Enter password:" )
-else:
+    password = input ( "Enter password: " )
+ else:
     for i in range(len(password)):
         print("*",end='')
+    valid_input = True
